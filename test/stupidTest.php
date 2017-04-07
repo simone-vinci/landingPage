33e291999350c96bd958fa47b77e0d9fdec7cf7a
simone-vinci <simovinc98@gmail.com>
2017-04-07 09:51:42 +0200

    Update stupidTest.php

--- test/stupidTest.php
+++ test/stupidTest.php
@@ -1,7 +1,9 @@
 <?php
     class stupidTest extends \PHPUnit_Framework_TestCase
     {
+        public function testTrueisTrue()
+        {
     $foo=true;
     $this->assertTrue($foo);
-    
+        }
     }
