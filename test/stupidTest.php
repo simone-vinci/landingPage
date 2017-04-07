c8aa00a01577061d6781f0076ef87fa4ea034056
simone-vinci <simovinc98@gmail.com>
2017-04-07 10:04:31 +0200

    Update stupidTest.php

--- test/stupidTest.php
+++ test/stupidTest.php
@@ -6,4 +6,10 @@
     $foo=true;
     $this->assertTrue($foo);
         }
+        public function testHTML()
+        {
+            $htmlOutput = '<p id="my_id"> ciao </p>';
+            $matcher = array('id'=>'my_id');
+            $this->assertTag($matcher, $htmlOutput);
+        }
     }
